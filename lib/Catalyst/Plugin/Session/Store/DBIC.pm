97fd9196392cba9d7d0378cee765617226fb2c8c
Daniel Westermann-Clark <danieltwc@cpan.org>
2006-07-31 18:38:47 +0000

    Revert change to set data to empty string if only setting expires, as it blanks the session data.  Whoops.

--- lib/Catalyst/Plugin/Session/Store/DBIC.pm
+++ lib/Catalyst/Plugin/Session/Store/DBIC.pm
@@ -168,11 +168,17 @@ sub store_session_data {
         $setting_expires = 1;
     }
 
-    $c->_dbic_session_resultset->update_or_create({
-        $config->{id_field}      => $key,
-        $config->{expires_field} => $c->session_expires,
-        $config->{data_field}    => $setting_expires ? '' : encode_base64(nfreeze($data)),
-    });
+    my %fields = (
+        $config->{id_field} => $key,
+    );
+    if ($setting_expires) {
+        $fields{$config->{expires_field}} = $c->session_expires;
+    }
+    else {
+        $fields{$config->{data_field}} = encode_base64(nfreeze($data));
+    }
+
+    $c->_dbic_session_resultset->update_or_create(\%fields);
 }
 
 =head2 delete_session_data
