96a1e405c6fe3452f06916dd599a64c306b1c816
Jonathan Yu <frequency@cpan.org>
2009-10-21 01:27:45 +0000

    Bring copyright statement in sync with the other packages

--- lib/Catalyst/Plugin/Session/Store/DBIC.pm
+++ lib/Catalyst/Plugin/Session/Store/DBIC.pm
@@ -336,7 +336,9 @@ Daniel Westermann-Clark E<lt>danieltwc@cpan.orgE<gt>
 
 =head1 COPYRIGHT
 
-Copyright 2006-2008 Daniel Westermann-Clark, all rights reserved.
+Copyright (c) 2006 - 2009
+the Catalyst::Plugin::Session::Store::DBIC L</AUTHOR>
+as listed above.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
